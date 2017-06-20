0dc5af066e65ed60d3181b593a2c355120bf641e
Einar Holsbø <einar@cs.uit.no>
2017-06-20 10:24:28 +0200

    forgot to commit this obviously

--- src/time_series_plot.R
+++ src/time_series_plot.R
@@ -48,13 +48,15 @@ year_limit <- c(min(shrinkage$year), max(shrinkage$year))
 rate_limit <- c(min(shrinkage$ebayes_lower, shrinkage$standard_lower),
                 max(shrinkage$ebayes_upper, shrinkage$standard_upper))
 
+pdf(file="timeseries.pdf", width=9, height=5)
 par(mfrow=c(2,3))
 for (town in c(worst_2016, best_2016)) {
   towndata <- filter(shrinkage, place==town)
   pop <- filter(towndata, year=="2016") %>% select(population) %>% .[[1]]
 
   plot(year_limit, rate_limit, type="n",
-       main=paste0(town, "\n(population: ", as.character(pop), ")"), xlab="", ylab="")
+       main=paste0(town, "\n(population: ", as.character(pop), ")"), xlab="", ylab="",
+       cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.2)
 
   points(towndata$year, towndata$rate, pch=20)
   lines(towndata$year, towndata$rate, lwd=2)
@@ -72,3 +74,25 @@ for (town in c(worst_2016, best_2016)) {
   }
 }
 
+dev.off()
+
+
+################################################################################
+#                               sticksplot
+################################################################################
+x <- shrinkage %>% filter(year==2016) %>% select(place, rate, ebayes_rate) %>%
+  mutate(place=(place=="Oslo kommune")) %>% arrange(place)
+rate_limit <- c(min(x[,-1]), max(x[,-1]))
+
+pdf(file="sticks.pdf", width=6, height=5)
+plot(rate_limit, c(1,2), type="n", main="Shrinkage vs ordinary estimates",
+     yaxt="n", ylab="", xlab="Crime rate estimate",
+     cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.2)
+a_ply(x, 1, function(r) {
+  color <- ifelse(r$place, "black", "grey")
+  rates <- c(r$rate, r$ebayes_rate)
+  yax <- c(1,2)
+  points(rates, yax, col=color, pch=19)
+  lines(rates, yax, lwd=3, col=color)
+})
+dev.off()
