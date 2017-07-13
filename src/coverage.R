52c5366cbe11d859d66351f0a27dcebf5f1630d1
Einar Holsbø <einar@cs.uit.no>
2017-07-13 20:46:40 +0200

    Fix experiment and figure

--- src/coverage.R
+++ src/coverage.R
@@ -28,31 +28,42 @@ covered <- function(x, true_rate) {
   c(ebayes_coverage=ebayes, standard_coverage=standard)
 }
 
-
-experiment <- raply(1E4, function() {
-  towns <- simulate_towns()
-  counts <- simulate_crimes(towns$population, towns$cpp, law="binomial")
-  data <- tibble(rate=towns$cpp, population=towns$population, reports=counts)
-  data <- as.tibble(cbind(data, stats(data)))
+crime_types <- norwegian_crime$crime_type %>% unique %>% sort
+experiment <- alply(crime_types, 1, function(ctype) {
+  towns <- mean_crimes(ctype)
+  rlply(5000, function() {
+    counts <- simulate_crimes(towns$population, towns$cpp, law="binomial")
+    data <- tibble(rate=counts/towns$population, population=towns$population, reports=counts)
+    data <- as.tibble(cbind(data, stats(data)))
     list(covered(data, towns$cpp), ctype)
+  }, .progress="text")
 }, .progress="text")
 
-errors <- cbind(experiment[, 2], experiment[, 1])
+
+
+exp2 <- NULL
+for (it in experiment) {
+  for (i in it) {
+    exp2 <- rbind(exp2, data.frame(eb=i[[1]][1], wld=i[[1]][2], type=i[[2]]))
+  }
+}
+
+errors <- exp2[exp2$type=="¬ Vold og mishandling", 1:2]
 
 pdf(file="coverage.pdf", width=6, height=5)
 plot(density(errors[,2]),type='n',
-     xlim=c(0.86, 0.99),
+     xlim=c(0.86, 0.96),
      ylim=c(0,45),
-     main='MC integration of coverage probabilities',
+     main='Distribution over all MC simulations',
      xlab='Coverage probability',
      cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.2)
 
 
-lines(density(errors[, 1]),lwd=2)
-abline(v=mean(errors[, 1]),lwd=2)
+lines(density(errors[, 2]),lwd=2)
+abline(v=mean(errors[, 2]),lwd=2)
 
-lines(density(errors[, 2]),lwd=2, col=2,lty=2)
-abline(v=mean(errors[, 2]),lwd=2, col=2, lty=2)
+lines(density(errors[, 1]),lwd=2, col=2,lty=2)
+abline(v=mean(errors[, 1]),lwd=2, col=2, lty=2)
 
 
 abline(v=0.95,lwd=2, col="grey")
