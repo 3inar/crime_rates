00884b9619f0e6a13b9d0e3659611828e62baa8f
Einar Holsbø <einar@cs.uit.no>
2017-08-11 15:17:07 +0200

    Simplified, more simulations

--- src/coverage.R
+++ src/coverage.R
@@ -1,9 +1,6 @@
 library(ProjectTemplate); load.project()
 library(plyr)
 
-
-
-
 covered <- function(x, true_rate) {
   ebayes <- mean(true_rate > x$ebayes_lower & true_rate < x$ebayes_upper)
   standard <- mean(true_rate > x$standard_lower & true_rate < x$standard_upper)
@@ -11,27 +8,22 @@ covered <- function(x, true_rate) {
   c(ebayes_coverage=ebayes, standard_coverage=standard)
 }
 
-crime_types <- norwegian_crime$crime_type %>% unique %>% sort
-experiment <- alply(crime_types, 1, function(ctype) {
-  towns <- mean_crimes(ctype)
-  rlply(5000, function() {
-    counts <- simulate_crimes(towns$population, towns$cpp, law="binomial")
-    data <- tibble(rate=counts/towns$population, population=towns$population, reports=counts)
-    data <- as.tibble(cbind(data, stats(data)))
-    list(covered(data, towns$cpp), ctype)
-  }, .progress="text")
+crime_type <- "¬ Vold og mishandling"
+nsim <- 1E5
+
+# experiment <- alply(crime_types, 1, function(ctype) {
+towns <- mean_crimes(crime_type)
+experiment <-  raply(nsim, function() {
+  counts <- simulate_crimes(towns$population, towns$cpp, law="binomial")
+  data <- tibble(rate=counts/towns$population, population=towns$population, reports=counts)
+  data <- as.tibble(cbind(data, stats(data)))
+  covered(data, towns$cpp)
 }, .progress="text")
+# }, .progress="text")
 
 
 
-exp2 <- NULL
-for (it in experiment) {
-  for (i in it) {
-    exp2 <- rbind(exp2, data.frame(eb=i[[1]][1], wld=i[[1]][2], type=i[[2]]))
-  }
-}
-
-errors <- exp2[exp2$type=="¬ Vold og mishandling", 1:2]
+errors <- experiment
 
 pdf(file="coverage.pdf", width=6, height=5)
 plot(density(errors[,2]),type='n',
@@ -42,10 +34,11 @@ plot(density(errors[,2]),type='n',
      cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.2)
 
 
-lines(density(errors[, 2]),lwd=2)
+bwd <- 0.002
+lines(density(errors[, 2], bw=bwd),lwd=2)
 abline(v=mean(errors[, 2]),lwd=2)
 
-lines(density(errors[, 1]),lwd=2, col=2,lty=2)
+lines(density(errors[, 1], bw=bwd),lwd=2, col=2,lty=2)
 abline(v=mean(errors[, 1]),lwd=2, col=2, lty=2)
 
 
